74a809550282a96f4ad7baca33cf6ac0a33c28fa
ymscorpion <isaac119mae@gmail.com>
2025-10-21 13:21:17 +0000

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:myapp/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
